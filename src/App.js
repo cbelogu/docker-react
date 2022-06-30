e4028eb5e65a8d712994b63638465d63d690810f
Loganathan Rathinaswamy <loganathan.rathinasw@tabcorp.com.au>
2022-06-30 11:03:50 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go back to learning Docker!
         </a>
       </header>
     </div>
